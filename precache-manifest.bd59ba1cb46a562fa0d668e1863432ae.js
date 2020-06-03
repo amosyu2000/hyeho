b41892b8dc58f6d77ab7eb3b4e0da2734f0cc03e
Amos Yu <amosyu2000@gmail.com>
2020-06-02 22:32:22 -0400

    Updates

---
+++ precache-manifest.bd59ba1cb46a562fa0d668e1863432ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d22bb31af17d0315ebc7a5941c1d657",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd446b68d5e5f3bcb021",
+    "url": "/static/css/main.83dcd51a.chunk.css"
+  },
+  {
+    "revision": "e00344894f0f1a3624e5",
+    "url": "/static/js/2.0bc8e1c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0bc8e1c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd446b68d5e5f3bcb021",
+    "url": "/static/js/main.3201e45c.chunk.js"
+  },
+  {
+    "revision": "afd6f368afc84edfaa22",
+    "url": "/static/js/runtime-main.6c1714ce.js"
+  },
+  {
+    "revision": "1085a2aee20a01157165e248f7823a22",
+    "url": "/static/media/TravelingTypewriter.1085a2ae.ttf"
+  }
+]);
